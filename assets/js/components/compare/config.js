88fc87ec55bd4d6821f4a6ec11f7bdef3c8e7907
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-02 20:39:24 +0100

    Compare page finished

--- assets/js/components/compare/config.js
+++ assets/js/components/compare/config.js
@@ -98,6 +98,7 @@ const runComparison = () => {
 
 	let scoreTally = [0, 0]; // [0] is left, [1] is right
 
+	// Determines which movie has a greater statistic number for each comparison item
 	leftSideStats.forEach((leftStat, index) => {
 		const rightStat = rightSideStats[index];
 		let leftSideValue = parseFloat(leftStat.dataset.value);
@@ -118,7 +119,44 @@ const runComparison = () => {
 			scoreTally[0]++;
 		}
 	});
-	console.log(scoreTally);
+	declareWinner(scoreTally);
+};
+
+const declareWinner = (score) => {
+	const comparisonContainer = document.querySelectorAll(
+		".comparison-items-container"
+	);
+
+	[leftMovieScore, rightMovieScore] = [...score];
+	[leftContainer, rightContainer] = [...comparisonContainer];
+
+	resetCardTallyDisplay(leftContainer);
+	resetCardTallyDisplay(rightContainer);
+
+	if (leftMovieScore === rightMovieScore) {
+		displayTieArticle(leftContainer, rightContainer);
+	} else if (leftMovieScore > rightMovieScore) {
+		displayWinOrLossArticle(leftContainer, rightContainer);
+	} else if (leftMovieScore < rightMovieScore) {
+		displayWinOrLossArticle(rightContainer, leftContainer);
+	}
+};
+
+const resetCardTallyDisplay = (container) => {
+	container.querySelector(".movie-win").classList.add("hide-element");
+	container.querySelector(".movie-loss").classList.add("hide-element");
+	container.querySelector(".movie-tie").classList.add("hide-element");
+};
+
+const displayTieArticle = (movie1, movie2) => {
+	movie1.querySelector(".movie-tie").classList.remove("hide-element");
+	movie2.querySelector(".movie-tie").classList.remove("hide-element");
+};
+
+// Toggle win/loss article card (win, loss)
+const displayWinOrLossArticle = (movie1, movie2) => {
+	movie1.querySelector(".movie-win").classList.remove("hide-element");
+	movie2.querySelector(".movie-loss").classList.remove("hide-element");
 };
 
 const applyComparisonColors = (lessThan, greaterThan) => {
@@ -176,13 +214,71 @@ const movieTemplate = (movieDetails) => {
 		imdbVotes,
 	} = movieDetails;
 
-	// Converting values
-	const dollars = parseInt(BoxOffice.replace(/\$/g, "").replace(/,/g, ""));
-	const metascore = parseInt(Metascore);
-	const rating = Number(imdbRating);
-	const votes = parseInt(imdbVotes.replace(/,/g, ""));
-	const awards = awardsValue(Awards);
+	try {
+		// Converting values
+		const dollars = parseInt(BoxOffice.replace(/\$/g, "").replace(/,/g, ""));
+		const metascore = parseInt(Metascore);
+		const rating = Number(imdbRating);
+		const votes = parseInt(imdbVotes.replace(/,/g, ""));
+		const awards = awardsValue(Awards);
+		return `
+		<article class="movie-info">
+			<figure class="movie-img-container">
+				<div class="movie-img">
+					<img src=${Poster}>
+				</div>
+			</figure>
+			<div class="movie-details">
+				<div class="content">
+					<h1>${Title}</h1>
+					<h4>${Genre}</h4>
+					<p>${Plot}</p>
+				</div>
+			</div>
+		</article>
+		<div class="comparison-items-container">
+			<article class="movie-win hide-element tally-card">
+				<p>Winner!</p>
+			</article>
+			<article class="movie-tie hide-element tally-card">
+				<p>Draw!</p>
+			</article>
+			<article class="movie-loss hide-element tally-card">
+				<p>Loser!</p>
+			</article>
+			<article data-value=${awards} class="comparison-item comparison-default">
+				<p class="title">${Awards}</p>
+				<p>Awards (${awards} Total)</p>
+			</article>
+			<article data-value=${dollars} class="comparison-item comparison-default">
+				<p class="title">${BoxOffice}</p>
+				<p>Box Office ($USD)</p>
+			</article>
+			<article data-value=${metascore} class="comparison-item comparison-default">
+				<p class="title">${Metascore}</p>
+				<p>Metascore (${Metascore}/100)</p>
+			</article>
+			<article data-value=${rating} class="comparison-item comparison-default">
+				<p class="title">${imdbRating}</p>
+				<p>IMDb Rating (${imdbRating}/10)</p>
+			</article>
+			<article data-value=${votes} class="comparison-item comparison-default">
+				<p class="title">${imdbVotes}</p>
+				<p>IMDb Votes</p>
+			</article>
+		</div>
+		`;
+	} catch (err) {
+		console.log("=============================");
+		console.log(`Caught Error!`);
+		console.log(`Search Caught With An Error: ${Title}`);
+		console.log(movieDetails);
+		console.log("=============================");
+		return displayBackupTemplate(Poster, Title, Genre, Plot);
+	}
+};
 
+const displayBackupTemplate = (Poster, Title, Genre, Plot) => {
 	return `
 	<article class="movie-info">
 		<figure class="movie-img-container">
@@ -198,24 +294,27 @@ const movieTemplate = (movieDetails) => {
 			</div>
 		</div>
 	</article>
-	<article data-value=${awards} class="comparison-item comparison-default">
-		<p class="title">${Awards}</p>
-		<p>Awards (${awards} Total)</p>
+	<article class="not-found">
+		<p><span class="black-highlight">Warning!</span> Statistics could not be found for this film/show.</p>
 	</article>
-	<article data-value=${dollars} class="comparison-item comparison-default">
-		<p class="title">${BoxOffice}</p>
-		<p>Box Office ($USD)</p>
+	<article class="comparison-item comparison-default">
+		<p class="title">Not Found</p>
+		<p>Awards</p>
 	</article>
-	<article data-value=${metascore} class="comparison-item comparison-default">
-		<p class="title">${Metascore}</p>
-		<p>Metascore (${Metascore}/100)</p>
+	<article class="comparison-item comparison-default">
+		<p class="title">Not Found</p>
+		<p>Box Office</p>
 	</article>
-	<article data-value=${rating} class="comparison-item comparison-default">
-		<p class="title">${imdbRating}</p>
-		<p>IMDb Rating (${imdbRating}/10)</p>
+	<article class="comparison-item comparison-default">
+		<p class="title">Not Found</p>
+		<p>Metascore</p>
 	</article>
-	<article data-value=${votes} class="comparison-item comparison-default">
-		<p class="title">${imdbVotes}</p>
+	<article class="comparison-item comparison-default">
+		<p class="title">Not Found</p>
+		<p>IMDb Rating</p>
+	</article>
+	<article class="comparison-item comparison-default">
+		<p class="title">Not Found</p>
 		<p>IMDb Votes</p>
 	</article>
 	`;
