a1bd959dd5c530b466bbabe4cd01899b97a7f5df
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-04 23:34:14 +0100

    Update utils.js

--- assets/js/main/utils.js
+++ assets/js/main/utils.js
@@ -9,3 +9,18 @@ const debounce = (func, delay = 1000) => {
 		}, delay);
 	};
 };
+
+const address = "http://127.0.0.1:5500",
+	keyJSON = "apikey.json";
+const apiKey = async () => {
+	return await fetch(`${address}/${keyJSON}`)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`Status Code ${res.status}`);
+			}
+
+			return res.json();
+		})
+		.then((data) => data.key)
+		.catch((err) => console.log(err));
+};
