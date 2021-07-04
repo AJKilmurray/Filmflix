f694367e3464acf2d8264bda964ffe5bdfe139c8
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-07-04 23:34:05 +0100

    Update searchbar.js

--- assets/js/components/search/searchbar.js
+++ assets/js/components/search/searchbar.js
@@ -1,7 +1,42 @@
-const search = (
+const search = ({
 	container,
 	renderSearchbar,
 	inputValidation,
 	renderResults,
-	onResultSelect
-) => {};
+	adjustCardInfoHeight,
+	onResultSelect,
+}) => {
+	const root = document.querySelector(".input-root");
+	renderSearchbar(root); // Renders searchbar
+
+	const search = document.querySelector(".searchbar");
+	const searchBtn = document.querySelector(".search-btn");
+
+	// On search button click
+	searchBtn.addEventListener("click", async (event) => {
+		event.preventDefault();
+
+		while (container.firstChild) {
+			container.removeChild(container.lastChild);
+		}
+
+		const searchResults = await inputValidation(search);
+
+		if (!searchResults) {
+			return;
+		}
+
+		for (let result of searchResults) {
+			const resultItem = document.createElement("div");
+			resultItem.classList.add("result-card");
+			const resultContent = renderResults(result);
+			resultItem.insertAdjacentHTML("afterbegin", resultContent);
+
+			container.appendChild(resultItem);
+		}
+		const resultCardInfo = document.querySelectorAll(".result-info");
+		adjustCardInfoHeight(resultCardInfo);
+
+		window.addEventListener("resize", adjustCardInfoHeight(resultCardInfo));
+	});
+};
